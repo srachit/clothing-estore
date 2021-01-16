08c7b77c39b0442c78212228fc81f445a082bde4
Rachit Sengupta <rachit_sengupta@intuit.com>
2021-01-16 21:05:45 +0400

    added react persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
 const middlerwares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlerwares));
+export const store = createStore(rootReducer, applyMiddleware(...middlerwares));
 
-export default store;
+export const persistor = persistStore(store);
